5300cdb1162f88cc2f5d42c5ce0d0583ccacad6f
Zdenek Hynek <zdenek.hynek@gmail.com>
2020-09-08 09:40:29 +0100

    Lint fix

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 
 import "./App.css";
 
-import UncontrolledLottie from "./components/uncontrolled_lottie";
 import ControlledLottie from "./components/controlled_lottie";
 
 function App() {
   return (
     <div className="App">
+
       <ControlledLottie />
     </div>
   );
